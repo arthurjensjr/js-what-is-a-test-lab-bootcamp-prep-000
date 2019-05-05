f630461d47885b5d101fc844d9b54c1a59bfcc23
aj.artwil <aj.artwil@gmail.com>
2019-05-05 20:41:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-// write your code below!
-var name = "Joe"
-var height = 74
-var message = ` is ${height} inches tall`
+function sayHello() {
+  console.log('Hello!')
+}
+sayHello()
 
-module.exports = {name, height, message}
